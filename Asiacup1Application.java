ad0b52c2eef872e7311c34d0ab88649f928d21d2
poojagulave <103043216+poojagulave@users.noreply.github.com>
2022-11-17 14:19:40 +0530

    Add files via upload

---
+++ Asiacup1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asiacup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asiacup1Application.class, args);
+	}
+
+}
